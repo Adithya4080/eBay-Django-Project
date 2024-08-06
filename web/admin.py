3dfed740d4a880b5a60ac1815646559e2dbc4cfb
adithya4080 <adithyas712@gmail.com>
2024-08-06 11:36:22 +0530

    update

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from web.models import Product, Category, Gallery
 
-# Register your models here.
+
+class GalleryAdmin(admin.TabularInline):
+    list_display = ["name","image"]
+    model = Gallery
+
+admin.site.register(Product)
+admin.site.register(Category)
